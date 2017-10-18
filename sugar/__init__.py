b5914c352dab1fdc77ae441155d7233994a88e92
ltoddy <1205446803@qq.com>
2017-10-18 09:13:43 +0800

    solving the loop import package problem

--- sugar/__init__.py
+++ sugar/__init__.py
@@ -1,8 +1,8 @@
 from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
 
-from .sugar import Sugar
-
 from ._context import session
 from ._context import request
 from ._context import current_app
+
+from .sugar import Sugar
